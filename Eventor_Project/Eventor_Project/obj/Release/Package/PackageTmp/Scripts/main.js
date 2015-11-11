2f764e151eb4060ecb1f5eef7551ad75edff1608
Samovit <samodelkinv@gmail.com>
2015-11-12 01:00:52 +0500

    without gitignore

---
+++ Eventor_Project/Eventor_Project/obj/Release/Package/PackageTmp/Scripts/main.js
@@ -0,0 +1,47 @@
+﻿
+$(document).ready(function () {
+    
+    var tabsChanged = function (event) {
+        var hash = window.location.hash;
+        if (hash) {
+            // Show/Hide Tabs
+            $('.tabs ' + hash).show().siblings().hide();
+            var currentClass = hash.replace('#', '.');
+            $('.tabs .tab-links ' + currentClass).addClass('active').siblings().removeClass('active');
+        }
+    };
+    tabsChanged();
+
+    window.addEventListener('hashchange', tabsChanged, false);
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        startDate: "+0d",
+        language: "ru",
+        autoclose: true
+    });
+});
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+};
+
+function ajaxReq(url, method, data, cb) {
+    $.ajax({
+        url: url,
+        type: method,
+        data: data,
+        success: function (data) {
+            cb(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            cb(null, jqXHR, textStatus, errorThrown);
+        }
+    });
+}
---
+++ Eventor_Project/Eventor_Project/obj/Release/Package/PackageTmp/Scripts/materials.js
@@ -0,0 +1,202 @@
+﻿$(document).ready(function () {
+    var MaterialModel = function (projectId) {
+        var self = this;
+        self.newMaterialTitle = ko.observable('');
+        self.newMaterialAmount = ko.observable('');
+        self.newMaterialProjectID = projectId;
+
+        self.materials = ko.observableArray();
+
+        self.addMaterial = function () {
+            var newMaterial = {
+                Title: self.newMaterialTitle,
+                RequiredAmount: self.newMaterialAmount,
+                ProjectId: self.newMaterialProjectID
+            }
+            ajaxReq('/Projects/AddMaterial', 'POST', newMaterial,
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        self.materials.push(data);
+                        self.newMaterialTitle('');
+                        self.newMaterialAmount('');
+                    }
+                    else {
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.getMaterials = function () {
+            ajaxReq('/Projects/GetMaterials', 'GET', { projectId: projectId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        debugger;
+                        data.forEach(function (item) {
+                            self.materials.push(item);
+                        });
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.removeMaterial = function (material) {
+            ajaxReq('/Projects/DeleteMaterial', 'POST', { materialId: material.MaterialId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        self.materials.remove(material);
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+    };
+
+    var CustomerModel = function (projectId) {
+        var self = this;
+        self.newCustomerRole = ko.observable('');
+        self.newCustomerDescription = ko.observable('');
+        self.newCustomerMaxCount = ko.observable('');
+        self.newCustomerMinCount = ko.observable('');
+        self.newCustomerProjectId = projectId;
+
+        self.customers = ko.observableArray();
+
+        self.addCustomer = function () {
+            var newCustomer = {
+                Role: self.newCustomerRole,
+                Description: self.newCustomerDescription,
+                MaxCount: self.newCustomerMaxCount,
+                MinCount: self.newCustomerMinCount,
+                ProjectId: self.newCustomerProjectId
+            }
+            debugger;
+            ajaxReq('/Projects/AddCustomer', 'POST', newCustomer,
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        self.customers.push(data);
+                        self.newCustomerRole('');
+                        self.newCustomerDescription('');
+                        self.newCustomerMaxCount('');
+                        self.newCustomerMinCount('');
+                    }
+                    else {
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.getCustomers = function () {
+            ajaxReq('/Projects/GetCustomers', 'GET', { projectId: projectId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        data.forEach(function (item) {
+                            self.customers.push(item);
+                        });
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.removeCustomer = function (customer) {
+            ajaxReq('/Projects/DeleteCustomer', 'POST', { customerId: customer.CustomerId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        self.customers.remove(customer);
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+    }
+
+    var OrganizerModel = function (projectId) {
+        var self = this;
+        self.newOrganizerName = ko.observable('');
+        self.newOrganizerDescription = ko.observable('');
+        self.newOrganizerRequiredCount = ko.observable('');
+        self.newOrganizerProjectId = projectId;
+
+        self.organizers = ko.observableArray();
+
+        self.addOrganizer = function () {
+            var newOrganizer = {
+                Name: self.newOrganizerName,
+                Description: self.newOrganizerDescription,
+                RequiredCount: self.newOrganizerRequiredCount,
+                ProjectId: self.newOrganizerProjectId
+            }
+            debugger;
+            ajaxReq('/Projects/AddOrganizer', 'POST', newOrganizer,
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        console.log(data);
+                        self.organizers.push(data);
+                        self.newOrganizerName('');
+                        self.newOrganizerDescription('');
+                        self.newOrganizerRequiredCount('');
+                    }
+                    else {
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.getOrganizers = function () {
+            ajaxReq('/Projects/GetOrganizers', 'GET', { projectId: projectId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        data.forEach(function (item) {
+                            self.organizers.push(item);
+                        });
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+
+        self.removeOrganizer = function (organizer) {
+            ajaxReq('/Projects/DeleteOrganizer', 'POST', { organizerId: organizer.OrganizerId },
+                function cb(data, jqXHR, textStatus, errorThrown) {
+                    if (data != null) {
+                        self.organizers.remove(organizer);
+                    }
+                    else {
+                        console.log(jqXHR);
+                        console.log(textStatus);
+                        console.log(errorThrown);
+                    }
+                });
+        };
+    }
+
+    var InventoryModel = function () {
+        var self = this;
+        var projectId = getQueryVariable('projectId');
+        self.materialModel = new MaterialModel(projectId);
+        self.customerModel = new CustomerModel(projectId);
+        self.organizerModel = new OrganizerModel(projectId);
+    }
+    var viewModel = new InventoryModel();
+    ko.applyBindings(viewModel);
+
+});
